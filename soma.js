7ed2b64c257cc47d434eb315d17d2d9cb3378ac3
Jackson Silva <jacksonmanoel1999@gmail.com>
2022-09-14 23:27:41 -0300

    exercício soma na pg html

---
+++ soma.js
@@ -0,0 +1,6 @@
+let num1 = parseInt(prompt('Digite um número:'))
+let num2 = parseInt(prompt('Digite mais um número:'));
+let resultado =(alert(`A soma é ${num1+num2}`));
+
+
+//exercício para soma na pg html via alert/prompt
